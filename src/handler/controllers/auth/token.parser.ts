19e7263518707b016e44ee7d6b41e2fb10bde4f5
Jacek Głogosz <jacek.glogosz@gmail.com>
2022-01-22 18:42:52 +0100

    auth headers switch

--- src/handler/controllers/auth/token.parser.ts
+++ src/handler/controllers/auth/token.parser.ts
@@ -1,8 +1,9 @@
-import { TokenParserInterface } from 'nestjs-jwt-authorize/lib/src';
 import { User } from 'src/handler/controllers/auth/user';
+import { BearerHeaderTokenParser } from 'nestjs-jwt-authorize/lib/src/logic/bearer-header-token.parser';
 
-export class TokenParser implements TokenParserInterface {
+export class TokenParser extends BearerHeaderTokenParser {
   getUserFromTokenObject(jwt: any): User {
+    console.log(jwt);
     return {
       roles: [],
     };
