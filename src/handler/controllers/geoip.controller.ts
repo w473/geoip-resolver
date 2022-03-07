a406f5ce73137e2b9a83a74af0b8c30bf9362c8c
Jacek Głogosz <jacek.glogosz@gmail.com>
2022-03-07 19:52:50 +0100

    feat(roles) role check removed

--- src/handler/controllers/geoip.controller.ts
+++ src/handler/controllers/geoip.controller.ts
@@ -4,7 +4,6 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GeoIPService } from 'src/application/services/geoip.service';
 import { IpDto } from 'src/handler/dtos/ip.dto';
 import { GeoIPResponseDto } from 'src/handler/dtos/geoip-response.dto';
-import { HasRole } from 'nestjs-jwt-authorize';
 
 @Controller('api/v1/geoip')
 @ApiTags('geoip')
@@ -12,7 +11,6 @@ export class GeoipController {
   constructor(private readonly geoIPService: GeoIPService) {}
 
   @Get(':ip')
-  @HasRole('SYS')
   @ApiResponse({
     type: GeoIPResponseDto,
     description: 'returns raw data from MMDB',
