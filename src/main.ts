608df8d2d989c535e23bcbe3b176e376621a7fd1
Jacek Głogosz <jacek.glogosz@gmail.com>
2022-03-07 20:16:37 +0100

    removed app guard

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(configService.get('PORT') ?? 3000);
+  await app.listen(configService.get('PORT') ?? 3001);
 }
 void bootstrap();
