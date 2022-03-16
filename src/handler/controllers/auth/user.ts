6bcdaa553966e4c114c529c373a5d4bcabba7857
Jacek Głogosz <jacek.glogosz@gmail.com>
2022-03-16 20:56:19 +0100

    cleanup

--- src/handler/controllers/auth/user.ts
+++
@@ -1,5 +0,0 @@
-import { UserInterface } from 'nestjs-jwt-authorize';
-
-export class User implements UserInterface {
-  roles: string[];
-}
--- src/handler/controllers/health.controller.ts
+++ src/handler/controllers/health.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 import { ApiTags } from '@nestjs/swagger';
-import { IsPublic } from 'nestjs-jwt-authorize';
 
 @ApiTags('health')
 @Controller('health')
